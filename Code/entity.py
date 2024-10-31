cd2780c1e93a5346ee02bc715effc8e3af914647
KassianoELL <kassi.edu11@gmail.com>
2024-10-31 12:24:42 -0300

    Adding classes generated from StarUml

---
+++ Code/entity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.Name = None
+        self.Surf = None
+        self.rect = None
+
+    def Move(self, ):
+        pass
---
+++ Code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.Window Surface = None
+        self.Name = None
+        self.Entity_list = None
+
+    def Run(self, ):
+        pass
