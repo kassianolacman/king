e409392461ae86ee62b9474d2ca0348b4b664063
KassianoELL <kassi.edu11@gmail.com>
2024-10-31 13:02:55 -0300

    creating Menu

--- Code/game.py
+++ Code/game.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from Code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.Window Surface = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 400))
 
-    def Run(self, ):
-        pass
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+            # check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close window
+                    quit()  # end pygame
--- Code/menu.py
+++ Code/menu.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 class Menu:
-    def __init__(self):
-        self.window surface = None
+    def __init__(self, window):
+        self.window = window
 
     def Run(self, ):
         pass
